b0661861b7a6037626c500c3ac84dae58c8d2b23
cmcahoon <cmcahoon@gmail.com>
2018-12-15 13:36:14 -0800

    chore: Migrate vehicle model unit test to Jest and Typescript.

--- test/unit/agency.test.ts
+++ test/unit/agency.test.ts
@@ -2,7 +2,7 @@ import { baseEndpointURL } from "../../src/constants";
 import * as agencyModel from "../../src/models/agency";
 
 const mock = require("./mock");
-const response = {
+const mockResponse = {
     body: [
         {
             display_name: "Test Agency",
@@ -23,21 +23,21 @@ describe("The agency model", () => {
     });
 
     it("should be able to list agencies", () => {
-        mock.get(baseEndpointURL + "/agencies/", () => response);
+        mock.get(baseEndpointURL + "/agencies/", () => mockResponse);
         return agencyModel.list().then((agencies) => {
-            expect(agencies).toEqual(response.body);
+            expect(agencies).toEqual(mockResponse.body);
         });
     });
 
     it("should be able to get a specific agency by id", () => {
-        mock.get(baseEndpointURL + "/agencies/", () => response);
-        return agencyModel.get(response.body[1].id).then((agency) => {
-            expect(agency).toEqual(response.body[1]);
+        mock.get(baseEndpointURL + "/agencies/", () => mockResponse);
+        return agencyModel.get(mockResponse.body[1].id).then((agency) => {
+            expect(agency).toEqual(mockResponse.body[1]);
         });
     });
 
     it("should return an error if you try to get an agency that does not exist", async () => {
-        mock.get(baseEndpointURL + "/agencies/", () => response);
+        mock.get(baseEndpointURL + "/agencies/", () => mockResponse);
         await expect(agencyModel.get("does-not-exist")).rejects.toThrow("Agency not found");
     });
 
